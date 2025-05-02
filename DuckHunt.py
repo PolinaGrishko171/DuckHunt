2b610415fe17636737b92d2851a2f3ccc3479171
OlexandrProkopchuk <sprok2005@gmail.com>
2025-05-02 14:08:13 +0300

    Change Duck size + game velocity

--- DuckHunt.py
+++ DuckHunt.py
@@ -5,17 +5,14 @@ import os
 
 pygame.init()
 
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Duck Hunt")
 
-
 WHITE = (255, 255, 255)
 BLUE = (135, 206, 250)
 BLACK = (0, 0, 0)
-
 
 font = pygame.font.SysFont(None, 36)
 
@@ -28,7 +25,11 @@ class Game:
         self.ducks = []
         self.ui_manager = UIManager(screen)
         self.is_running = True
-        self.spawn_timer = 0
+        self.last_spawn_time = 0
+        self.spawn_interval = 2000
+        self.difficulty_timer = 0
+        self.difficulty_step = 500
+        self.min_spawn_interval = 400
 
     def start(self):
         self.run()
@@ -41,25 +42,33 @@ class Game:
                 self.player.shoot(event.pos, self.ducks)
 
     def spawn_duck(self):
-        if random.random() < 0.05:  
+        now = pygame.time.get_ticks()
+        if now - self.last_spawn_time >= self.spawn_interval:
             self.ducks.append(Duck())
+            self.last_spawn_time = now
+
+    def update_difficulty(self):
+        now = pygame.time.get_ticks()
+        if now - self.difficulty_timer >= 3000:
+            if self.spawn_interval > self.min_spawn_interval:
+                self.spawn_interval -= self.difficulty_step
+            Duck.increase_speed()
+            self.difficulty_timer = now
 
     def update(self):
-        for duck in self.ducks:
+        for duck in self.ducks[:]:
             duck.move()
             if duck.is_hit:
                 self.ducks.remove(duck)
                 self.player.score += 1
 
-        
         self.ducks = [duck for duck in self.ducks if 0 <= duck.x <= SCREEN_WIDTH]
 
-        
         if self.player.misses >= 5:
             self.game_over()
 
     def draw(self):
-        self.screen.fill(BLUE)  
+        self.screen.fill(BLUE)
         for duck in self.ducks:
             duck.draw(self.screen)
         self.ui_manager.draw_score(self.player.score)
@@ -73,13 +82,14 @@ class Game:
         self.is_running = False
 
     def run(self):
+        self.difficulty_timer = pygame.time.get_ticks()
         while self.is_running:
             self.handle_events()
             self.spawn_duck()
+            self.update_difficulty()
             self.update()
             self.draw()
             self.clock.tick(60)
-
 
 
 class Player:
@@ -95,14 +105,17 @@ class Player:
         self.misses += 1
 
 
-
 class Duck:
+    speed_increase = 0
+
     def __init__(self):
         self.x = random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 100)
         self.y = random.randint(50, SCREEN_HEIGHT - 50)
-        self.speed_x = random.choice([3, 4])
+        base_speed = random.choice([3, 4]) + Duck.speed_increase
+        self.speed_x = base_speed
         self.speed_y = random.choice([1, -1]) * random.randint(1, 3)
-        self.image = pygame.image.load(os.path.join(os.path.dirname(__file__), "duck.png"))
+        original_image = pygame.image.load(os.path.join(os.path.dirname(__file__), "duck.png"))
+        self.image = pygame.transform.scale(original_image, (80, 80))       
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
         self.is_hit = False
 
@@ -114,6 +127,9 @@ class Duck:
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
+    @classmethod
+    def increase_speed(cls):
+        cls.speed_increase += 0.5
 
 
 class UIManager:
@@ -133,9 +149,6 @@ class UIManager:
         self.screen.blit(gameover_text, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 - 50))
 
 
-
 game = Game(screen)
 game.start()
-
 pygame.quit()
-
